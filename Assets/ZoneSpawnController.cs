fa054b68af14862f13eb3775dd007426cb019f8c
Paul Leelaviwatana <apaulture@gmail.com>
2019-09-20 14:11:05 -0700

    - Added spawn zone - Changed platform to a square for measuring distance

---
+++ Assets/ZoneSpawnController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneSpawnController : MonoBehaviour
+{
+    public GameObject note;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 position = transform.position + (3 * transform.up);
+        print(position);
+        Instantiate(note, position, Quaternion.identity, gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
