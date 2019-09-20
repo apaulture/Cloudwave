86b8a54d7d7bed6ae65ada82595db20f673aa7ed
Paul Leelaviwatana <apaulture@gmail.com>
2019-09-19 22:14:20 -0700

    - Added haptic feedback when you touch the note - Added animation to note when touched - Duplicated 2 scenes for Anthony and Steven - Cleaned up unused assets

---
+++ Assets/Scripts/NoteController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour
+{
+    Renderer m_Renderer;
+    Material m_Material;
+    AudioSource m_AudioSource;
+    Animator m_Animator;
+
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        m_Material = m_Renderer.material;
+        m_AudioSource = GetComponent<AudioSource>();
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_Animator.SetBool("IsTouched", true);
+
+
+        if (other.gameObject.CompareTag("Hand"))
+        {
+            PlayNote();
+
+            // Touched with which hand?
+            switch (other.gameObject.name)
+            {
+                case "LeftHandAnchor":
+                    OVRInput.SetControllerVibration(0.5f, 1f, OVRInput.Controller.LTouch);
+                    break;
+                case "RightHandAnchor":
+                    OVRInput.SetControllerVibration(1f, 1f, OVRInput.Controller.RTouch);
+                    break;
+            }
+        }
+    }
+
+    void PlayNote()
+    {
+        m_AudioSource.Play();
+    }
+}
