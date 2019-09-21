029f181c114434161d347acd61abb6681162d288
XZ-Crazy <pallittoxz@gmail.com>
2019-09-20 17:30:19 -0700

    Very basic note spawning scripts

---
+++ Assets/Scripts/Song.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Song : MonoBehaviour
+{
+    public float bpm;
+    public AudioClip song;
+    public Spawner temp;
+    private float timeCounter;
+    private float nextNoteTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+        if(timeCounter >= nextNoteTime)
+        {
+            temp.spawnNote();
+            nextNoteTime += 60 / bpm;
+        }
+    }
+}
