75c72e333cad67292d5ac3b35357d3fa75a5ae36
Paul Leelaviwatana <apaulture@gmail.com>
2019-09-28 16:43:47 -0700

    - Latest stable update #2

---
+++ Assets/Scripts/SwipeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeController : MonoBehaviour
+{
+    Collider m_ParentCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ParentCollider = GetComponentInParent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (transform.GetChild(0).gameObject.activeSelf == false)
+        {
+            m_Collider.enabled = true;
+        }
+        */
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Enable parent collider");
+        m_ParentCollider.enabled = true;
+    }
+}
