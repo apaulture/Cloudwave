62e8faed28ddf30b7665bcaf0eb2f1872d15a38c
XZ-Crazy <pallittoxz@gmail.com>
2019-10-19 16:51:06 -0700

    Added ScoreCounter systems

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+
+    private Image image;
+    public float progress;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = progress;
+    }
+}
