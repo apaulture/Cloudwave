90162b84a8ff6d9d4d860ad95389a1d3af0f1c40
XZ-Crazy <pallittoxz@gmail.com>
2019-11-18 23:42:11 -0800

    Added more particle systems and half finished the audio preprocessing

---
+++ Assets/Scripts/Synthesizer/BeatChecker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatChecker : MonoBehaviour
+{
+    public float fluxThreshold;
+    public List<float> beatTimes;
+    public int totalNotes;
+    public float nextTime;
+
+    void FindTheGoodBeats(List<SpectralFluxInfo> songData)
+    {
+        float runningTotal = 0f;
+        int count = 0;
+
+        List<SpectralFluxInfo> peaks = new List<SpectralFluxInfo>();
+
+        foreach(SpectralFluxInfo pointInfo in songData)
+        {
+            if(pointInfo.isPeak)
+            {
+                runningTotal += pointInfo.spectralFlux;
+                count++;
+                peaks.Add(pointInfo);
+            }
+        }
+        fluxThreshold = runningTotal / count;
+        foreach(SpectralFluxInfo pointInfo in peaks)
+        {
+            if (pointInfo.spectralFlux >= fluxThreshold)
+                beatTimes.Add(pointInfo.time);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (beatTimes[0] >= Time.time)
+        {
+            transform.SetPositionAndRotation(new Vector3(0f, 0f, 0f), new Quaternion(0f, 0f, 0f, 0f));
+            beatTimes.Remove(beatTimes[0]);
+        }
+    }
+}
